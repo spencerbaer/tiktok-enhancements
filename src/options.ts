fef3f28da94cde226a630d18ae35cf2898edbd14
Spencer Baer <spencer@spencerbaer.com>
2022-12-26 17:10:53 -0600

    show fetch errors

--- src/options.ts
+++ src/options.ts
@@ -6,10 +6,20 @@ const favorites_count = document.getElementById("favorites-count")
 const refresh_favorites = document.getElementById("refresh-favorites")
 
 refresh_favorites.addEventListener("click", async () => {
-    const response = await fetch("http://10.0.0.154:3000/tiktok")
-    const favorites = await response.json()
 
-    await setStorageItem("favorites", favorites)
+    try {
+        const response = await fetch("http://10.0.0.154:3000/tiktok")
+
+        if (!response.ok) {
+            console.error(`Refresh failed ${response.status}`)
+        }
+        const favorites = await response.json()
+    
+        await setStorageItem("favorites", favorites)
+    } catch(e) {
+        console.error(`Refresh failed: ${e}`)
+    }
+    
 })
 
 async function updateFavoriteCount() {
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -3,7 +3,7 @@ import { initializeStorageWithDefaults } from './storage';
 chrome.runtime.onInstalled.addListener(async () => {
   // Here goes everything you want to execute after extension initialization
 
-  await chrome.storage.local.clear()
+  // await chrome.storage.local.clear()
 
   await initializeStorageWithDefaults({
     favorites: ['7161970556839955754', '7162425838074531118']
